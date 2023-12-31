35df5e55617251a31e00a4f38096dc40c1ad4f82
Ruterm <155300134+Ruterm@users.noreply.github.com>
2023-12-31 16:40:41 +0200

    Add files via upload
    mod add cooked apple

---
+++ cooked_apple.js
@@ -0,0 +1,16 @@
+elements.cooked_apple = {
+	color: "#bf7c49",
+	behavior: behaviors.POWDER,
+	category: "food",
+	state: "solid",
+	density: 539,
+	temp: 0,
+	stateHigh: "ash",
+	tempHigh: 200,
+	tempLow: -273,
+	stateLow: "cooked_apple",
+	hardness: 0,
+	burn: 0,
+	burnInto: "ash"	
+	}
+};
