b17c000e0eab11d9a3d9e6646ffac059fc03f7a2
Ruterm <155300134+Ruterm@users.noreply.github.com>
2023-12-31 17:34:11 +0200

    Update (1.3) cooked_apple.js
    add iced_apple

--- cooked_apple.js
+++ cooked_apple.js
@@ -1,13 +1,26 @@
 //В этом моде я добавил печёное яблоко
 elements.cooked_apple = {
-	color: "#bf7c49",
-	behavior: behaviors.POWDER,
-	category: "food",
-	state: "solid",
-	density: 539,
-	temp: 0,
-	stateHigh: "ash",
-	tempHigh: 200,
-	tempLow: -273,
-	burnInto: "ash"	
+    color: "#271f1c",
+    behavior: behaviors.POWDER,
+    temp: 20,
+    category: "food",
+    state: "solid",
+    tempHigh: 345,
+    stateHigh: "ash",
+    tempLow: -30,
+    stateLow: iced_apple,
+    density: 475
 };
+
+elements.iced_apple = {
+	color: "#f7a37e",
+	behavior: behaviors.WALL,
+	temp: 20,
+	category: "states",
+	state: "solid",
+	tempHigh: 10,
+	stateHigh: "cooked_apple",
+	density: 745
+};
+		
+	
